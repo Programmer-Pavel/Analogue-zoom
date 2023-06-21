f268c9991a5d61a6c860a416bf4becb407edf1c1
@Programmer-Pavel <pavelkukuskmn388@gmail.com>
2023-06-21 06:48:34 +0300

    feat: add init files

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path"); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+    entry: "./src/index.js", // Точка входа для сборки проекта
+
+    output: {
+        filename: "bundle.js", // Имя выходного файла сборки
+        path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                ],
+                include: /\.module\.css$/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /\.module\.css$/,
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"), // Каталог для статики
+        },
+        open: true, // Автоматически открывать браузер
+    },
+
+    mode: "development", // Режим сборки
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = "production";
+    } else {
+        config.mode = "development";
+    }
+    return config;
+};
