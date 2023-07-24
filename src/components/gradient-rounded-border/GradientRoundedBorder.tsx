f5d7a2cd6b774bcab751fd2b0a83841253e83cfb
@Programmer-Pavel <pavelkukuskmn388@gmail.com>
2023-07-24 06:43:05 +0300

    chore: adjust webpack configuration

---
+++ src/components/gradient-rounded-border/GradientRoundedBorder.tsx
@@ -0,0 +1,6 @@
+import styles from "./GradientRoundedBorder.module.scss";
+import React from "react";
+
+export const GradientRoundedBorder = () => {
+    return <div className={styles.main}>dd</div>;
+};
