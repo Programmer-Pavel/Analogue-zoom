45bbf6e7c01b23ae3d74c8be0d4dcd1ca50e021d
pavel <pavelkukuskmn388@gmail.com>
2024-06-07 15:13:14 +0300

    feat: сделал возможность отправить сообщением файлы (картинки) в чате

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,51 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import Tabs from '@mui/material/Tabs';
-import Box from '@mui/material/Box';
-import { Messages } from './Messages';
-import { Tab } from '@mui/material';
-import { useSocketIo } from '../../hooks/useSocketIo';
-import { useAuth } from '../../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Box from "@mui/material/Box";
+import { Messages } from "./Messages";
+import { Tab } from "@mui/material";
+import { useSocketIo } from "../../hooks/useSocketIo";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Chat = () => {
   const [value, setValue] = useState<number | boolean>(false);
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   const [choosedUserId, setChoosedUserId] = useState<number | null>(null);
 
-  const { socketConnection, isConnected }  = useSocketIo()
-  const { infoFromToken } = useAuth()
+  const { socketConnection, isConnected } = useSocketIo();
+  const { infoFromToken } = useAuth();
 
   useEffect(() => {
-      if(isConnected) {
-        socketConnection?.emit("getUsers");
+    if (isConnected) {
+      socketConnection?.emit("getUsers");
 
-        //@ts-expect-error fix
-        function onGetUsers(usersFromServer: any) {
-            if(infoFromToken) {
-               const usersWithoutCurrentUser = usersFromServer.filter((el: any) => el.id !== infoFromToken.userId);
-               setUsers(usersWithoutCurrentUser);
-            }
+      //@ts-expect-error fix
+      function onGetUsers(usersFromServer: any) {
+        if (infoFromToken) {
+          const usersWithoutCurrentUser = usersFromServer.filter(
+            (el: any) => el.id !== infoFromToken.userId
+          );
+          setUsers(usersWithoutCurrentUser);
         }
-  
-        socketConnection?.on('users', onGetUsers);
-
-        return () => {
-          socketConnection?.off('users', onGetUsers);
-        };
       }
-  }, [isConnected])
+
+      socketConnection?.on("users", onGetUsers);
+
+      return () => {
+        socketConnection?.off("users", onGetUsers);
+      };
+    }
+  }, [isConnected]);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
   const onChooseUser = (userId: number) => {
-    setChoosedUserId(userId)
+    setChoosedUserId(userId);
     socketConnection?.emit("getCurrentUserMessages", userId);
   };
 
   return (
     <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '100%' }}
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: "100%",
+      }}
     >
       <Tabs
         orientation="vertical"
@@ -53,34 +60,40 @@ export const Chat = () => {
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
+        sx={{ borderRight: 1, borderColor: "divider" }}
       >
-        {
-          users?.map((el: any) => (
-            <Tab 
-              key={el.id} 
-              value={el.id} 
-              label={<div>{ infoFromToken.userId === el.id ? `${el.username} (MY)` : el.username}</div> } 
-              onClick={() => onChooseUser(el.id)}
-            />
-          ))
-        }
+        {users.length
+          ? users?.map((el: any) => (
+              <Tab
+                key={el.id}
+                value={el.id}
+                label={
+                  <div>
+                    {infoFromToken.userId === el.id
+                      ? `${el.username} (MY)`
+                      : el.username}
+                  </div>
+                }
+                onClick={() => onChooseUser(el.id)}
+              />
+            ))
+          : "Нет пользователей"}
       </Tabs>
-      {
-        choosedUserId 
-        ? <Box width='90%' p='5px'>
-          <Messages choosedUserId={choosedUserId}/>
-        </Box> 
-        : <Box
-            display="flex"
-            alignItems='center'
-            justifyContent='center'
-            width='90%'
-            p='5px'
-          >
-            Select a chat to start messaging
+      {choosedUserId ? (
+        <Box width="90%" p="5px">
+          <Messages choosedUserId={choosedUserId} />
         </Box>
-      }
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="90%"
+          p="5px"
+        >
+          Select a chat to start messaging
+        </Box>
+      )}
     </Box>
   );
-}
+};
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,142 +1,217 @@
-import React, { useEffect, useState } from 'react'
-import { useSocketIo } from '../../hooks/useSocketIo'
-import { useAuth } from '../../hooks/useAuth'
+import React, { useEffect, useRef, useState } from 'react'
 import Box from '@mui/material/Box'
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import IconButton from '@mui/material/IconButton';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import { styled } from '@mui/material/styles';
+import AttachFileIcon from '@mui/icons-material/AttachFile'
+import Button from '@mui/material/Button'
+import SendIcon from '@mui/icons-material/Send'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import { styled } from '@mui/material/styles'
+import { useAuth } from '../../hooks/useAuth'
+import { useSocketIo } from '../../hooks/useSocketIo'
+import { ChoosedFilesModalShow } from './ChoosedFilesModal'
+import { TextField } from '@mui/material'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography';
 
-const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
-  });
+const filesToObjectUrl = (copyMessage: any) => {
+  const fileUrls = copyMessage.files?.map((el: any) => {
+    const blob = new Blob([el], { type: 'application/octet-stream' });
+    const fileUrl = URL.createObjectURL(blob);
+    return fileUrl
+  })
 
-export const Messages = ({ choosedUserId }: { choosedUserId: number | null }) => {
-    const [sendMessageInputValue, setSendMessageInputValue] = useState('')
-    const [allMessages, setAllMessages] = useState<any>([])
+  copyMessage["fileUrls"] = fileUrls
+}
 
-    const { socketConnection }  = useSocketIo()
-    const { infoFromToken } = useAuth()
+export const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+})
 
-    useEffect(() => {
-        socketConnection?.emit("getMessages", choosedUserId);
+export function Messages({
+  choosedUserId,
+}: {
+  choosedUserId: number | null
+}) {
+  const [sendMessageInputValue, setSendMessageInputValue]
+    = useState<string>('')
+  const [allMessages, setAllMessages] = useState<any>([])
+  const [choosedFilesModalShow, setChoosedFilesModalShow] = useState<boolean>(false)
+  const [file, setFile] = useState<any>()
 
-        function onGetMessages(messagesFromServer: any) {
-            setAllMessages(messagesFromServer);
+  const { socketConnection } = useSocketIo()
+  const { infoFromToken } = useAuth()
+
+  const fileUploadRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    socketConnection?.emit('getMessages', choosedUserId)
+
+    function onGetMessages(messagesFromServer: any) {
+      const messagesWithFilesUrl = messagesFromServer.map((el: any) => {
+        if(el.files) {
+          const copyMessage = { ...el }
+          filesToObjectUrl(copyMessage)
+          return copyMessage
         }
-
-        function onGetNewMessage(newMessage: any) {
-            if(choosedUserId === newMessage.toUserId || choosedUserId === newMessage.userId)
-                setAllMessages((lastState: any) => [...lastState, newMessage]);
-        }
-  
-        socketConnection?.on('messages', onGetMessages);
-  
-        socketConnection?.on('newMessage', onGetNewMessage);
-
-        return () => {
-            socketConnection?.off('messages', onGetMessages);
-            socketConnection?.off('newMessage', onGetNewMessage);
-        };
-    }, [choosedUserId])
-
-    const onSendMessageInputChange = (e: any) => {
-        console
-        setSendMessageInputValue(e.target.value)
+        return el
+      })
+      setAllMessages(messagesWithFilesUrl)
     }
 
-    const onSendMessageInput = () => {
-        if(sendMessageInputValue) {
-            socketConnection?.emit('userMessageSend', {
-                message: sendMessageInputValue,
-                toUserId: choosedUserId
-            })
+    function onGetNewMessage(newMessage: any) {
+      if (
+        choosedUserId === newMessage.toUserId
+        || choosedUserId === newMessage.userId
+      ) {
+        const copyMessage = { ...newMessage }
+
+        if(newMessage.files) {
+          filesToObjectUrl(copyMessage)
         }
-        setSendMessageInputValue('')
+        setAllMessages((lastState: any) => [...lastState, copyMessage])
+      }
     }
 
-    const uploadFile = (e: any) => {
-        socketConnection?.emit("uploadFile", {
-            fileName: e.target.files[0].name,
-            file: e.target.files[0]
-        });
-    }
+    socketConnection?.on('messages', onGetMessages)
 
-    return (
-        <Box
-            height='100%'
-            display='flex' 
-            flexDirection='column' 
-            justifyContent='space-between'
-        >
-            <Box display='flex' flexDirection='column' gap='5px'>
-                {
-                    allMessages.map((el: any, index: number) => {
-                        const isMe = infoFromToken.userId === el.userId
-                        return (
-                            <Box 
-                                key={index} 
-                                display='flex' 
-                                justifyContent={ isMe ? 'flex-end' : 'flex-start' }
-                            >
-                                <Box 
-                                    border='1px solid black' 
-                                    borderRadius={ isMe ? '10% 10% 0% 10%' : '10% 10% 10% 0%' }
-                                    p='5px'
-                                    bgcolor={ isMe ? '#ACE1AF' : '#B2FFFF'}
-                                >
-                                    { isMe ? 'I' : `user ${el.userId}` }: { el.message }
-                                </Box>
-                            </Box>
-                        )
-                    })
-                }
+    socketConnection?.on('newMessage', onGetNewMessage)
+
+    return () => {
+      socketConnection?.off('messages', onGetMessages)
+      socketConnection?.off('newMessage', onGetNewMessage)
+    }
+  }, [choosedUserId])
+
+  const onSendMessageInputChange = (e: any) => {
+    setSendMessageInputValue(e.target.value)
+  }
+
+  const handleCloseChoosedFilesModal = () => {
+    setChoosedFilesModalShow(false)
+    if (fileUploadRef.current)
+      fileUploadRef.current.value = ''
+  }
+
+  const onSendMessageInput = () => {
+    if (sendMessageInputValue) {
+      socketConnection?.emit('userMessageSend', {
+        message: sendMessageInputValue,
+        toUserId: choosedUserId,
+      })
+    }
+    setSendMessageInputValue('')
+  }
+
+  const uploadFile = (e: any) => {
+    if (e.target.files.length) {
+      setChoosedFilesModalShow(true)
+      const uploadedfile = e.target.files[0]
+      const imageUrl = URL.createObjectURL(uploadedfile)
+      const file = {
+        src: imageUrl,
+        name: uploadedfile.name,
+        type: uploadedfile.type,
+        file: uploadedfile,
+        toUserId: choosedUserId,
+      }
+      setFile(file)
+    }
+  }
+
+  return (
+    <Box
+      height="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <Box display="flex" flexDirection="column" gap="5px" overflow='auto' pr='10px'>
+        {allMessages.map((el: any, index: number) => {
+          const isMe = infoFromToken.userId === el.userId
+          return (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={isMe ? 'flex-end' : 'flex-start'}
+            >
+              <Box
+                border="1px solid black"
+                borderRadius={isMe ? '4% 4% 0% 4%' : '4% 4% 4% 0%'}
+                p="10px"
+                bgcolor={isMe ? '#ACE1AF' : '#B2FFFF'}
+              >
+                {isMe ? 'I' : `user ${el.userId}`}
+                :
+                <Box display='flex' gap='8px' mb='4px'>
+                  { 
+                    el.fileUrls?.map((el: any) => (
+                       <Avatar key={el} sx={{ width: 200, height: 200 }} variant="rounded" src={el} />
+                    ))
+                  }
+                </Box>
+                <Typography variant="body1" gutterBottom ml='5px'>
+                  {el.message}
+                </Typography>
+              </Box>
             </Box>
-            <Box display='flex' gap='5px' mt='10px'>
-                <TextField
-                    autoComplete=''
-                    fullWidth 
-                    variant="outlined" 
-                    placeholder='write a message...' 
-                    value={sendMessageInputValue} 
-                    onChange={onSendMessageInputChange}
-                    InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">
-                                <IconButton 
-                                    size="large" 
-                                    component="label"
-                                    role={undefined}
-                                    tabIndex={-1}
-                                >
-                                    <AttachFileIcon />
-                                    <VisuallyHiddenInput type="file" onChange={uploadFile}/>
-                                </IconButton>
-                          </InputAdornment>
-                        ),
-                      }}
-                />
-
-                <Button 
-                    sx={{ width: '150px' }}
-                    variant="contained" 
-                    endIcon={<SendIcon />} 
-                    onClick={onSendMessageInput}
+          )
+        })}
+      </Box>
+      <Box display="flex" gap="5px" mt="10px">
+        <TextField
+          autoComplete=""
+          fullWidth
+          variant="outlined"
+          placeholder="write a message..."
+          value={sendMessageInputValue}
+          onChange={onSendMessageInputChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton
+                  size="large"
+                  component="label"
+                  role={undefined}
+                  tabIndex={-1}
                 >
-                    Send
-                </Button>
-            </Box>
-        </Box>
-    )
+                  <AttachFileIcon />
+                  <VisuallyHiddenInput
+                    ref={fileUploadRef}
+                    type="file"
+                    onChange={uploadFile}
+                  />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <Button
+          sx={{ width: '150px' }}
+          variant="contained"
+          endIcon={<SendIcon />}
+          onClick={onSendMessageInput}
+        >
+          Send
+        </Button>
+      </Box>
+
+      {choosedFilesModalShow && (
+        <ChoosedFilesModalShow
+          handleCloseChoosedFilesModal={handleCloseChoosedFilesModal}
+          choosedFilesModalShow={choosedFilesModalShow}
+          file={file}
+          choosedUserId={choosedUserId}
+        />
+      )}
+    </Box>
+  )
 }
