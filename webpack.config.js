ec26397103c1ebfc82325c1adbbc9df3189f9f8c
pavel <pavelkukuskmn388@gmail.com>
2024-06-24 10:38:24 +0300

    chore: пофиксил пару ошибок babel, typescript

--- webpack.config.js
+++ webpack.config.js
@@ -97,13 +97,6 @@ const config = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript',
-            ],
-          },
         },
       },
     ],
