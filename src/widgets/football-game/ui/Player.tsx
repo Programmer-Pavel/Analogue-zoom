25690abf6dc9000dba79874e8f776b5f83b909aa
pavel <pavelkukuskmn388@gmail.com>
2024-07-26 14:19:44 +0300

    feat: добавил силу удара по мячу

--- src/widgets/football-game/ui/Player.tsx
+++ src/widgets/football-game/ui/Player.tsx
@@ -1,10 +1,12 @@
-import { forwardRef, useRef } from 'react'
+import { forwardRef, useRef, useState } from 'react'
 import { KeyboardControls } from '@react-three/drei'
 import Ecctrl from 'ecctrl'
 import { type CollisionEnterPayload, type CollisionExitPayload, vec3 } from '@react-three/rapier'
 
 export const Player = forwardRef<any, any>((props, ref) => {
+  const keyDPressedRef = useRef(false)
   const playerRef = useRef<any>(null)
+  const pressStartTimeRef = useRef<any>(null)
 
   const keyboardMap = [
     { name: 'forward', keys: ['ArrowUp'] },
@@ -16,25 +18,50 @@ export const Player = forwardRef<any, any>((props, ref) => {
   ]
 
   const keydownEventFunc = (e: KeyboardEvent) => {
-    if (e.key === 'd') {
-      const position = vec3()
-      playerRef.current.getWorldDirection(position)
-      position.normalize().multiplyScalar(20)
+    if (e.key === 'd' && !keyDPressedRef.current) {
+      pressStartTimeRef.current = Date.now()
+      keyDPressedRef.current = true
+    }
+  }
+
+  const keyupEventFunc = (e: KeyboardEvent) => {
+    if (e.key === 'd' && keyDPressedRef.current) {
+      const direction = vec3()
+      playerRef.current.getWorldDirection(direction)
+
+      const pressDuration = (Date.now() - pressStartTimeRef.current) / 10
+      let duration
+
+      if (pressDuration > 50) {
+        duration = 50
+      }
+      else if (pressDuration < 20) {
+        duration = 20
+      }
+      else {
+        duration = pressDuration
+      }
+
+      direction.normalize().multiplyScalar(duration)
 
       // @ts-expect-error fix
-      ref.current.applyImpulse({ x: position.x, y: position.y, z: position.z }, true)
+      ref.current.applyImpulse({ x: direction.x, y: direction.y, z: direction.z }, true)
+
+      keyDPressedRef.current = false
     }
   }
 
   const onCollisionEnter = ({ other }: CollisionEnterPayload) => {
     if (other.rigidBodyObject.name === 'ball') {
       document.addEventListener('keydown', keydownEventFunc)
+      document.addEventListener('keyup', keyupEventFunc)
     }
   }
 
   const onCollisionExit = ({ other }: CollisionExitPayload) => {
     if (other.rigidBodyObject.name === 'ball') {
       document.removeEventListener('keydown', keydownEventFunc)
+      document.removeEventListener('keyup', keydownEventFunc)
     }
   }
 
